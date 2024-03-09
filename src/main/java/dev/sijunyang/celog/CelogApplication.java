f249253450d50001adcbac3c3245cefb00ce1515
YangSiJun528 <yangsijun5528@gmail.com>
2024-03-09 15:36:08 +0900

    feat: init project

---
+++ src/main/java/dev/sijunyang/celog/CelogApplication.java
@@ -0,0 +1,13 @@
+package dev.sijunyang.celog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelogApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CelogApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/sijunyang/celog/CelogApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sijunyang.celog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelogApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
