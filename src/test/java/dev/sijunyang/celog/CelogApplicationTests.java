4fb59b4a8387abb4aad057adee0d09308c99075b
Sijun Yang <yangsijun5528@gmail.com>
2024-03-21 22:52:16 +0900

    test: 통합테스트 비활성화

--- src/test/java/dev/sijunyang/celog/CelogApplicationTests.java
+++ src/test/java/dev/sijunyang/celog/CelogApplicationTests.java
@@ -2,8 +2,9 @@ package dev.sijunyang.celog;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 
-@SpringBootTest
+//@SpringBootTest 아직 통합테스트를 위한 외부 서비스(MySQL, Redis) 구성이 되지 않았음
 class CelogApplicationTests {
 
     @Test
