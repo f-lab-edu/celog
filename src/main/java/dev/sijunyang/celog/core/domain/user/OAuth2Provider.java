84dd3a94a0b3bb56ef997bce5532e760a7dde3c1
Sijun Yang <yangsijun5528@gmail.com>
2024-03-29 13:52:00 +0900

    feat: OAuth2Provider 구현

---
+++ src/main/java/dev/sijunyang/celog/core/domain/user/OAuth2Provider.java
@@ -0,0 +1,13 @@
+package dev.sijunyang.celog.core.domain.user;
+
+public final class OAuth2Provider {
+
+    /**
+     * OAuth 인증 기능을 제공하는 Github 서비스의 이름입니다.
+     */
+    public static final String GITHUB = "github";
+
+    private OAuth2Provider() {
+    };
+
+}
