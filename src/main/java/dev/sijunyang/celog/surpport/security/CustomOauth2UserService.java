57ad0665be29775bef83167bd56438cf0856a81e
Sijun Yang <yangsijun5528@gmail.com>
2024-04-03 23:18:42 +0900

    refactor: `@Service` 대신 `@Bean`으로 주입

--- src/main/java/dev/sijunyang/celog/surpport/security/CustomOauth2UserService.java
+++ src/main/java/dev/sijunyang/celog/surpport/security/CustomOauth2UserService.java
@@ -17,15 +17,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
 
-@Service
 @RequiredArgsConstructor
 public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
 
@@ -37,7 +34,7 @@ public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     private final UserService userService;
 
-    private final DefaultOAuth2UserService delegateOauth2UserService = new DefaultOAuth2UserService();
+    private final OAuth2UserService<OAuth2UserRequest, OAuth2User> delegateOauth2UserService;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
