6c73b65cbff39a0aaaf30fe866b530b2473259c9
Sijun Yang <yangsijun5528@gmail.com>
2024-04-08 16:18:40 +0900

    style: format 형식에 맞게 수정

--- src/test/java/dev/sijunyang/celog/surpport/ncp/ImageUploadServiceImplIntegrationTest.java
+++ src/test/java/dev/sijunyang/celog/surpport/ncp/ImageUploadServiceImplIntegrationTest.java
@@ -15,43 +15,45 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 // 통합 테스트 구현, 실제 요청이 발생하고 있고, 지금은 통합테스트 환경이 구축되지 않아 CI 시 실패하므로 주석처리하였음
-//@SpringBootTest
-//class ImageUploadServiceImplIntegrationTest {
+// @SpringBootTest
+// class ImageUploadServiceImplIntegrationTest {
 //
-//    @Autowired
-//    private ImageUploadServiceImpl service;
+// @Autowired
+// private ImageUploadServiceImpl service;
 //
-//    @Autowired
-//    private S3Template template;
+// @Autowired
+// private S3Template template;
 //
-//    @Autowired
-//    private NcpProperties ncpProperties;
+// @Autowired
+// private NcpProperties ncpProperties;
 //
-//    @Test
-//    void shouldUploadImage() {
-//        // Given
-//        String fileLocation = "./src/test/java/dev/sijunyang/celog/surpport/ncp/test-image.jpg";
-//        Resource resource = new FileSystemResource(fileLocation);
+// @Test
+// void shouldUploadImage() {
+// // Given
+// String fileLocation =
+// "./src/test/java/dev/sijunyang/celog/surpport/ncp/test-image.jpg";
+// Resource resource = new FileSystemResource(fileLocation);
 //
-//        // When
-//        String result = this.service.execute(resource);
+// // When
+// String result = this.service.execute(resource);
 //
-//        // Then
-//        assertNotNull(result);
+// // Then
+// assertNotNull(result);
 //
-//        // 테스트가 성공했으므로 필요없는 데이터는 삭제
-//        String filename = StringUtils.getFilename(StringUtils.uriDecode(result, StandardCharsets.UTF_8));
-//        template.deleteObject(ncpProperties.bucketName(), filename);
-//    }
+// // 테스트가 성공했으므로 필요없는 데이터는 삭제
+// String filename = StringUtils.getFilename(StringUtils.uriDecode(result,
+// StandardCharsets.UTF_8));
+// template.deleteObject(ncpProperties.bucketName(), filename);
+// }
 //
-//    @Test
-//    void shouldThrowInvalidInputExceptionWhenFileExtensionIsNotSupported() {
-//        // Given
-//        String fileLocation = "./src/test/java/dev/sijunyang/celog/surpport/ncp/test-text.txt";
-//        Resource resource = new FileSystemResource(fileLocation);
+// @Test
+// void shouldThrowInvalidInputExceptionWhenFileExtensionIsNotSupported() {
+// // Given
+// String fileLocation = "./src/test/java/dev/sijunyang/celog/surpport/ncp/test-text.txt";
+// Resource resource = new FileSystemResource(fileLocation);
 //
-//        // When & Then
-//        assertThrows(InvalidInputException.class, () -> service.execute(resource));
-//    }
+// // When & Then
+// assertThrows(InvalidInputException.class, () -> service.execute(resource));
+// }
 //
-//}
+// }
