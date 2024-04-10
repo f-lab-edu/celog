5df0aa81be77d3a3d705517afa9688162ffbdb6d
Sijun Yang <yangsijun5528@gmail.com>
2024-04-10 20:42:10 +0900

    style: 포멧 맞게 수정

--- src/test/java/dev/sijunyang/celog/surpport/ncp/TestIntegrationTest.java
+++ src/test/java/dev/sijunyang/celog/surpport/ncp/TestIntegrationTest.java
@@ -16,7 +16,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
-@SpringBootTest(classes = {CelogApplication.class})
+@SpringBootTest(classes = { CelogApplication.class })
 class TestIntegrationTest {
 
     @Autowired
