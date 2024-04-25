718f01c0c703d5923bf2a2cfdc55012494cac77d
Sijun Yang <yangsijun5528@gmail.com>
2024-04-26 01:06:07 +0900

    refactor: OrNull 변수 제거 및 주석 변경
    너무 과한 처리였다고 생각한다.
    
    이후에 여러 번 or 오래 사용되는 거면 모를까

--- src/main/java/dev/sijunyang/celog/surpport/security/AuthenticatedUserManagerImpl.java
+++ src/main/java/dev/sijunyang/celog/surpport/security/AuthenticatedUserManagerImpl.java
@@ -21,16 +21,12 @@ public class AuthenticatedUserManagerImpl implements AuthenticatedUserManager {
 
     @Override
     public Long getId() {
-        Authentication authenticationOrNull = SecurityContextHolder.getContext().getAuthentication();
-        if (authenticationOrNull == null || authenticationOrNull instanceof AnonymousAuthenticationToken) {
-            throw new AuthenticationCredentialsNotFoundException(
-                    "사용자를 식별할 수 없습니다. authentication: " + authenticationOrNull);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            throw new AuthenticationCredentialsNotFoundException("사용자를 식별할 수 없습니다. authentication: " + authentication);
         }
 
-        // 이제 null이 확실하게 아니므로 변수명에서 OrNull 제거.
-        Authentication authentication = authenticationOrNull;
-
-        // 인증 된 사용자의 이름(명)을 가져와 Long 타입으로 변환하여 반환합니다.
+        // 인증 된 사용자의 이름 속성을 가져와 Long 타입으로 변환하여 반환합니다.
         if (authentication instanceof OAuth2AuthenticationToken) {
             OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
             return Long.valueOf(oAuth2User.getName());
@@ -42,17 +38,13 @@ public class AuthenticatedUserManagerImpl implements AuthenticatedUserManager {
 
     @Override
     public Role getRole() {
-        Authentication authenticationOrNull = SecurityContextHolder.getContext().getAuthentication();
-        if (authenticationOrNull == null || authenticationOrNull instanceof AnonymousAuthenticationToken) {
-            throw new AuthenticationCredentialsNotFoundException(
-                    "사용자를 식별할 수 없습니다. authentication: " + authenticationOrNull);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+            throw new AuthenticationCredentialsNotFoundException("사용자를 식별할 수 없습니다. authentication: " + authentication);
         }
 
-        // 이제 null이 확실하게 아니므로 변수명에서 OrNull 제거.
-        Authentication authentication = authenticationOrNull;
-
-        // 인증 된 사용자의 "role" 속성 값을 가져와 Role 타입으로 반환합니다.
-        // 인증 된 사용자는 항상 "role" 속성 값을 가지고 있습니다.
+        // Authentication 객체를 가져와 Role 객체를 가져온다.
+        // 인증 된 사용자는 항상 속성 값으로 Role 객체를 가지고 있다.
         if (authentication instanceof OAuth2AuthenticationToken) {
             OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
             return oAuth2User.getAttribute(CustomOauth2UserService.CELOG_ROLE);
