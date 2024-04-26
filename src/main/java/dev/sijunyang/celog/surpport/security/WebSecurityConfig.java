a05d43022915cbcd01b6ec8a9e74dd087c39a031
양시준 <yangsijun5528@gmail.com>
2024-04-26 16:41:49 +0900

    feat: 테스트 전용 Header 기반 인증기능 구현 (#53)

--- src/main/java/dev/sijunyang/celog/surpport/security/WebSecurityConfig.java
+++ src/main/java/dev/sijunyang/celog/surpport/security/WebSecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +22,8 @@ public class WebSecurityConfig {
 
     private final AccessDeniedHandler accessDeniedHandler;
 
+    private final HeaderAuthenticationFilter headerAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.formLogin(AbstractHttpConfigurer::disable)
@@ -28,6 +31,7 @@ public class WebSecurityConfig {
             // CSRF 토큰을 사용하는게 안전하나, 실제 서비스가 아니므로 당장은 신경쓰기 않기로 함
             .csrf(AbstractHttpConfigurer::disable)
             .oauth2Login(Customizer.withDefaults())
+            .addFilterAfter(this.headerAuthenticationFilter, CorsFilter.class)
             .exceptionHandling((configurer) -> configurer.accessDeniedHandler(this.accessDeniedHandler)
                 .authenticationEntryPoint(this.authenticationEntryPoint))
             .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.anyRequest().permitAll());
